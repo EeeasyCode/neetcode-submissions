d08710716e1c2c97c1df12a55b69e24f94525ed4
Khan / 이창민 <EeeasyCode@gmail.com>
2026-05-19 23:18:35 +0900

    Add: anagram-groups - submission-15

---
+++ Data Structures & Algorithms/anagram-groups/submission-15.kt
@@ -0,0 +1,20 @@
+class Solution {
+    fun groupAnagrams(strs: Array<String>): List<List<String>> {
+        // 1. strs 배열을 하나씩 순회한다. (str)
+        // 2. str을 정렬한 뒤, map에 key를 보고 append 한다. (getOrPut)
+        // 3. 순회가 끝나면 map의 각 value를 반환한다.
+        val dict = mutableMapOf<String, MutableList<String>>()
+
+        for (str in strs) {
+            val sortedStr = str.toCharArray().sorted().joinToString("")
+
+            if (sortedStr !in dict) {
+                dict.put(sortedStr, mutableListOf())
+            }
+
+            dict[sortedStr]!!.add(str)
+        }
+
+        return dict.values.toList()
+    }
+}
