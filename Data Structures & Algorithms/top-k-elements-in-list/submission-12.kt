df22e4128f0ac30d26015edc790ea53da0e67510
Khan / 이창민 <EeeasyCode@gmail.com>
2026-05-19 23:34:22 +0900

    Add: top-k-elements-in-list - submission-12

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-12.kt
@@ -0,0 +1,20 @@
+class Solution {
+    fun topKFrequent(nums: IntArray, k: Int): IntArray {
+        // 1. nums 를 순회하면서 map value 에 count를 찍어준다.
+        // 2. 순회가 끝나면 value 기준 내림차순 정렬 후 상위 k를 출력한다.
+
+        // key: num, value: count
+        val dict = mutableMapOf<Int, Int>()
+
+        for (num in nums) {
+            dict[num] = (dict[num] ?: 0) + 1
+        }
+
+        return dict
+            .entries
+            .sortedByDescending{ it.value }
+            .take(k)
+            .map{ it.key }
+            .toIntArray()
+    }
+}
